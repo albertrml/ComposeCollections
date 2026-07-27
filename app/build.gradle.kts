64f9c129ef3f628c355aaa4768d014cb95832b1f
arml <albertrml.dev@gmail.com>
2026-07-27 14:48:47 -0300

    Merge remote-tracking branch 'origin/main'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    `maven-publish`
 }
 
 android {
@@ -31,6 +32,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release") {
+            publishApk()
+        }
+    }
 }
 
 dependencies {
@@ -56,4 +63,31 @@ dependencies {
     debugImplementation(composeBom)
     debugImplementation(libs.androidx.ui.test.manifest)
     debugImplementation(libs.androidx.ui.tooling)
+}
+
+group = "br.com.arml"
+version = "0.1.0"
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            groupId = "br.com.arml.composecollections"
+            artifactId = "composecollections"
+            version = "0.1.0"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/albertrml/ComposeCollections")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
